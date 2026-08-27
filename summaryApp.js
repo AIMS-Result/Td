49b4e84cf851c295d6eb26fac2bbed7b1041558d
AIMS-Result <sumitnaman771@gmail.com>
2026-08-27 17:53:23 +0530

    Update registryCsvUrl to new Google Sheets link

--- summaryApp.js
+++ summaryApp.js
@@ -6,7 +6,7 @@ app.controller('SummaryController', function($scope, $http) {
     // CONFIGURATION LINKS
     // =========================================================
     var diaryCsvUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSf7RKtiK4aD2NnEgocCJ4pngXV4KuxXWoRD_CwJ3JoyW8h_MA/formResponse';
-    var registryCsvUrl = 'YOUR_NEW_STANDALONE_REGISTRY_CSV_LINK_HERE';
+    var registryCsvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTKsJnIL98uvAr-RpzFN8Ozx6H73IAqYqfLpSmI1hM5PzWCvpdMe9ZofWbmbmo8229Up044R3F6kcdm/pub?gid=0&single=true&output=csv';
     // =========================================================
 
     $scope.isLoading = true;
